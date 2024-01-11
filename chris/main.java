f29e04c940865f33334ccd4393cf33cf745dcc6b
Chris V <74806407+Kristouillle@users.noreply.github.com>
2024-01-11 14:28:09 -0500

    Update main.java
    changer pour du "vrai" code
    -cv-

--- chris/main.java
+++ chris/main.java
@@ -1,7 +1,5 @@
-class Main
-  {
-    public static void main()
-    {
-      
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Aweille les boys!!");
     }
-  }
+}
